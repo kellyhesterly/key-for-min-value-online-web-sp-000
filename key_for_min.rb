d287f97eb94d3d4f0c1c44bdb66b1baac507d9fd
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-02 19:40:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-def key_for_min_value(people)
-  people.each do |name, amount|
-  if amount == 10
-      return name
-  elsif amount == 1
-      return name
-  else
-    return nil
-    end
-  end
+
+def key_for_min_value(name_hash)
 end
