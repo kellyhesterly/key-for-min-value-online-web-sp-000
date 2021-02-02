eb5aa86cb8540042bb4ac8fd08a71ab7710431d3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-02 19:41:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if lowest_value == 0 || lowest_value < value
       lowest_value = value
+      lowest_key = name
     end
   end
 end
